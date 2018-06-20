2c625dcb11d5e17bf1cb57900ecfac58501522c0
Michael Blandford <mdblandford@gmail.com>
2018-06-20 21:00:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,12 +2,12 @@ def sort_array_asc(array)
   array.sort {|a,b| a<=>b}
 end
 
-def sort_array_desc(integer)
-  integer.sort! {|a,b| b<=>a}
+def sort_array_desc(array)
+  array.sort! {|a,b| b<=>a}
 end
 
-def sort_array_char_count(string)
-  string.sort {|a,b| a.length<=>b.length}
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length<=>b.length}
 end
 
 def swap_elements(array)
